ed7d9dda2224a72ad0911d2e0524e297013f0ed9
Vallerga <antoniovallerga85@gmail.com>
2023-04-23 04:25:55 +0200

    bond utenti with dipendenti

--- src/main/java/com/gestione/dispositivi/aziendali/entity/Utente.java
+++ src/main/java/com/gestione/dispositivi/aziendali/entity/Utente.java
@@ -3,6 +3,8 @@ package com.gestione.dispositivi.aziendali.entity;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.gestione.dispositivi.aziendali.model.Dipendente;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
@@ -38,6 +41,8 @@ public class Utente {
 	@Column(nullable = false, unique = true)
 	private String email;
 	private String password;
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "utente")
+    private Dipendente dipendente;
 	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
 	@JoinTable(name = "utenti_ruoli",
 			joinColumns = @JoinColumn(name = "utente_id", referencedColumnName = "id"),
--- src/main/java/com/gestione/dispositivi/aziendali/model/Dipendente.java
+++ src/main/java/com/gestione/dispositivi/aziendali/model/Dipendente.java
@@ -2,12 +2,15 @@ package com.gestione.dispositivi.aziendali.model;
 
 import java.util.List;
 
+import com.gestione.dispositivi.aziendali.entity.Utente;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -32,6 +35,8 @@ public class Dipendente {
     private String email;
     @Column(nullable = false)
     private String password;
+    @OneToOne(mappedBy = "dipendente")
+    private Utente utente;
 
     @ManyToMany
     private List<Dispositivo> lista_dispositivi;
